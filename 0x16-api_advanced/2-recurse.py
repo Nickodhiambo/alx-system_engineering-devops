0c8ad16c8a83a0b7de6075ff343254b47d9d8286
Nick <nodhiambo01@gmail.com>
2023-11-07 07:25:11 -0800

    Recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""
+Contains a function that recursively calls a reddit API,
+retrieving all hot posts to a subreddit
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of all hot posts on a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    # set up appropriate headers
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    # set up params
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    # Send a GET request to API and store response
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 404:
+        return None
+    # Extract part of the response
+    results = response.json().get('data')
+    after = results.get('after')
+    count += results.get('dist')
+    for item in results.get('children'):
+        hot_list.append(item.get('data').get('title'))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
